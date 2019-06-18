3eedfbd3284aacf0945b6642299febaaa55ca46c
Jordan Bacon <jmbacon92@gmail.com>
2019-06-18 07:54:26 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,8 +2,13 @@
 
 def loop_iterator(number_of_times)
 phrase = "Welcome to Flatiron School's Web Development Course!"
-
-
+count = 0
+loop do
+  puts phrase
+    count += 1
+    break if count = number_of_times
+  end
+end
 
 number_of_times.times do
   puts phrase
